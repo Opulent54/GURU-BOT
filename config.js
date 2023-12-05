8997f0941b493e1be18ae49ac83dec6ee15239e2
Opulent <78411012+Opulent54@users.noreply.github.com>
2023-12-05 12:45:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919863815726', 'Neptune Bot', true],
+  ['916009512884', 'Sumit', true], 
   [''] 
 ] //Number of owners
 
